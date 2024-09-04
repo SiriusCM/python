0a2b920101dca0ede4f9391cc35d315193c4dd6e
SiriusCM <963937371@qq.com>
2024-09-04 15:10:51 +0800

    tap刷新信息

--- tap/mysql.py
+++ tap/mysql.py
@@ -4,6 +4,70 @@ from itertools import groupby
 import redis
 
 
+def select_info(db, userId, humanId):
+    baseMap = select(db, 'select name,level from game_human_base_0 where id = ' + humanId)
+    questSnList = select(db,
+                         'select questSn from game_human_quest_0 where humanId = ' + humanId + ' and state = 2')
+    personaNum = select(db, 'select count(*) from game_human_persona_0 where humanId = ' + humanId)
+    achievementNum = select(db, 'select count(*) from game_human_achievement_0 where humanId = ' + humanId)
+    unionBossScore = select(db, 'select totalScore from game_human_union_boss_base_0 where id = ' + humanId)
+    dcRankSn = select(db, 'select rankSn from game_human__d_c__base_info_0 where id = ' + humanId)
+
+    thiefList = select(db,
+                       'select id,humanId,sn,level as `等级`,awakeLevel as `觉醒等级` from game_human_thief_0 where humanId = ' + humanId)
+    weaponList = select(db, '''SELECT game_human_weapon_0.humanId,game_human_weapon_0.id,game_human_weapon_0.sn,game_human_thief_0.weaponLevel AS `等级`,game_human_weapon_0.remouldCount AS `改造等级`
+                    FROM game_human_weapon_0 LEFT JOIN game_human_thief_0 ON game_human_weapon_0.humanId = game_human_thief_0.humanId and game_human_thief_0.sn = game_human_weapon_0.thiefSn
+                    WHERE game_human_weapon_0.humanId = ''' + humanId + ' and game_human_weapon_0.sn in ' + weapon_condition)
+    coopList = select(db,
+                      'select id,humanId,cooperatorSn as sn,level as `等级` from game_human_open_world_coop_0 where humanId = ' + humanId)
+
+    confQuest = None
+    for quest in questSnList:
+        confQuest = ConfQuest[quest['questSn']]
+        if confQuest.type == 1:
+            break
+
+    confChallengeRank = ConfChallengeRank[dcRankSn[0]['rankSn']]
+    confRankRewardGroup = ConfRankRewardGroup[confChallengeRank.rankGroup]
+    rank = redis_db.zrevrank('difficult_challenge_rank_list:110-' + str(confChallengeRank.rankLevel),
+                             str(humanId))
+    if rank is None:
+        rank = '1等星1阶'
+    else:
+        rank = rank * 100 / eval('dc_card' + str(confChallengeRank.rankLevel))
+        rank = int(rank)
+        for confRankReward in confRankRewardGroup:
+            if rank >= confRankReward.rankInterval0 and rank <= confRankReward.rankInterval1:
+                rank = confRankReward.rankText
+                break
+
+    for thief in thiefList:
+        confThief = ConfThief[thief['sn']]
+        thief['名称'] = confThief.name
+        thief['星级'] = confThief.gradeInitial
+        del thief['humanId']
+
+    for weapon in weaponList:
+        if weapon['等级'] is None:
+            weapon['等级'] = 1
+        confWeapon = ConfWeapon[weapon['sn']]
+        weapon['名称'] = confWeapon.name
+        weapon['星级'] = confWeapon.quality
+        del weapon['humanId']
+
+    for coop in coopList:
+        confCoop = ConfCoop[coop['sn']]
+        coop['名称'] = confCoop.name
+        del coop['humanId']
+
+    data = {'role_id': userId, 'role_name': baseMap[0]['name'], 'level': baseMap[0]['level'],
+            '怪盗数量': len(thiefList), '人格面具': personaNum[0]['count(*)'], '成就': achievementNum[0]['count(*)'],
+            '心之海段位': rank,
+            '公会Boss总分': unionBossScore[0]['totalScore'], '玩家主线进度': confQuest.name, '拥有怪盗信息': thiefList,
+            '拥有武器信息': weaponList, '拥有协同者信息': coopList}
+    return data
+
+
 def select_role(db, condition, accounts):
     roles = select(db, 'select id,name as `role_name`,level from game_human_base_0 where id in ' + condition)
     for role in roles:
@@ -111,13 +175,6 @@ def select_thief(db, condition, accounts):
 
 
 def select_weapon(db, condition, accounts):
-    weapon_condition = '('
-    for sn in ConfWeapon:
-        confWeapon = ConfWeapon[sn]
-        if confWeapon.quality == 5 or confWeapon.quality == 6:
-            weapon_condition = weapon_condition + str(confWeapon.sn) + ','
-    weapon_condition = weapon_condition[0:- 1]
-    weapon_condition = weapon_condition + ')'
     weapons = select(db, '''select game_human_weapon_0.humanId,game_human_weapon_0.id,game_human_weapon_0.sn,game_human_thief_0.weaponLevel as 'field_2',
                 game_human_weapon_0.remouldCount as 'field_4' from game_human_weapon_0 left join game_human_thief_0 on game_human_weapon_0.humanId = game_human_thief_0.humanId 
                 and game_human_thief_0.sn = game_human_weapon_0.thiefSn where game_human_weapon_0.humanId in ''' + condition + ' and game_human_weapon_0.sn in ' + weapon_condition)
@@ -200,6 +257,14 @@ ConfWeapon = load_conf('ConfWeapon.json')
 ConfCoop = load_conf('ConfCoop.json')
 ConfChallengeRank = load_conf('ConfChallengeRank.json')
 ConfRankReward = load_conf('ConfRankReward.json')
+
+weapon_condition = '('
+for sn in ConfWeapon:
+    confWeapon = ConfWeapon[sn]
+    if confWeapon.quality == 5 or confWeapon.quality == 6:
+        weapon_condition = weapon_condition + str(sn) + ','
+weapon_condition = weapon_condition[0:- 1] + ')'
+
 ConfRankRewardGroup = {}
 for sn in ConfRankReward:
     confRankReward = ConfRankReward[sn]
---
+++ tap/web.py
@@ -0,0 +1,39 @@
+import pymysql
+import redis
+from flask import Flask, request
+from pymysql import cursors
+
+import mysql
+
+ips = ['10.148.154.122', '10.148.154.92', '10.148.154.47', '10.148.154.105', '10.148.154.54', '10.148.154.65',
+       '10.148.154.127', '10.148.154.34', '10.148.154.52',
+       '10.148.154.21', '10.148.154.111', '10.148.154.41', '10.148.154.117', '10.148.154.43', '10.148.154.19',
+       '10.148.154.36', '10.148.154.33',
+       '10.148.154.35', '10.148.154.38', '10.148.154.18', '10.148.154.31', '10.148.154.23', '10.148.154.45',
+       '10.148.154.42', '10.148.154.48']
+ips = ['10.77.38.188']
+
+redis_db = redis.Redis(host='10.77.38.188', port=10480, password='Jpu2COnXTI43lWi1')
+
+app = Flask(__name__)
+
+weaponCondition = mysql.weapon_condition
+print(weaponCondition)
+
+
+@app.route('/tap/getTapData')
+def hello():
+    userId = request.args.get('userId')
+    humanId = redis_db.hget('userid2id', userId)
+    humanId = humanId.decode()
+    index = int(humanId) % 2
+    db = pymysql.connect(cursorclass=cursors.DictCursor, host=ips[int(index / 2)], user='root',
+                         password='N2kH5lJVJLAHWObs',
+                         database='persona5_15_game_' + str(index))
+    ret = mysql.select_info(db, userId, humanId)
+    db.close()
+
+    return ret
+
+
+app.run(port=8888)
